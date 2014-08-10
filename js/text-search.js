afb0f7a807f5e895e13e097acb9c041784773fcb
Peter Shin <peshin@ebay.com>
2014-08-10 08:05:40 -0700

    fixing bugs and styles.

--- js/text-search.js
+++ js/text-search.js
@@ -6,14 +6,13 @@
 		$displayEl = null,
 		$selected = null,
 		caseSensitive = true;
-   
+
 	$(document).on('click','#text-search-extension li',selectCallback);
 
 	initDocEvents(document);
 
 	function selectCallback(){
 		var $this = $(this),
-			height = $this.parent().height(),
 			data = $this.data();
 
 		clearSelect();
@@ -23,8 +22,8 @@
 		data.textEl.html(data.textEl.html().replace(data.input,'<span class="ts-ce-hl">' + data.input + '</span>'))
 
 		$('html, body').animate({
-          scrollTop: data.offset.top - height - 20
-          },400);		
+          scrollTop: data.textEl.offset().top - 100
+          },400);
 	}
 
 	function clearSelect(){
@@ -32,11 +31,11 @@
 			var data = $selected.data();
 			data.textEl.html(data.textEl.html().replace('<span class="ts-ce-hl">' + data.input + '</span>',data.input))
 			$selected.removeClass('selected');
-		}		
+		}
 	}
 
 	function isVisible(element) {
-	  while (element) {    
+	  while (element) {
 	    style = window.getComputedStyle(element);
 	    if (style && (style.getPropertyValue('display') == 'none' ||
 	                  style.getPropertyValue('visibility') == 'hidden'))
@@ -50,7 +49,8 @@
 		$(doc).on('keydown',function(evt){
 			if(evt.which === 191){
 				toggleMenu();
-			}	
+				return false;
+			}
 		});
 	}
 
@@ -60,12 +60,13 @@
 
 	function initSearchEvents(){
 		 $displayEl.on('keydown','input', function(evt){
+		 	evt.stopPropagation();
 
 		 	if(keypressHandler){
 		 		clearTimeout(keypressHandler);
 		 	}
 
-		 	var $this = $(this); 
+		 	var $this = $(this);
 
 		 	keypressHandler = setTimeout(function(){
 
@@ -90,11 +91,11 @@
 				}
 
 				for(var i=0; i<results.length; i++){
-					var $parent = $(results[i]).parent()
-						offset = $parent.offset(),
-						content = results[i].data.replace(input,'<span class="ts-ce-hl">' + input + '</span>');
+					var $parent = $(results[i]).parent();
 
-					$li = $('<li>' + content + '</li>');
+					$li = $('<li></li>');
+					$li.text(results[i].data)
+						.html($li.html().replace(input,'<span class="ts-ce-hl">' + input + '</span>'))
 
 					if(!$selected){
 						$selected = $li;
@@ -102,7 +103,6 @@
 					}
 
 					$li.data({
-						offset: offset,
 						textEl: $parent,
 						input: input
 					});
@@ -117,7 +117,7 @@
 		 	}, 50);
 
 
-		 });
+		 }).on('click','i',toggleMenu);
 	}
 
  	//update with regex
@@ -135,13 +135,13 @@
 	function toggleMenu(){
 		$displayEl = $('#text-search-extension');
 		if($displayEl.length === 0){
-			$displayEl = $('<div id="text-search-extension"><input type="text" id="search" placeholder="Find in page"></input><ul></ul></div>');
+			$displayEl = $('<div id="text-search-extension"><div class="search-wrap"><input type="text" id="search" placeholder="Find in page" autocomplete="off"></input><i></i></div><ul></ul></div>');
 			$('body').after($displayEl);
 			initSearchEvents();
 			$displayEl.find('input').focus();
 		} else {
-			$displayEl.toggleClass('$displayEl');
-			if(!$displayEl.hasClass($displayEl)){
+			$displayEl.toggleClass('hide-text-search');
+			if(!$displayEl.hasClass('hide-text-search')){
 				$displayEl.find('input').focus();
 			}
 		}
