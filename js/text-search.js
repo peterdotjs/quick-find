e21eef68ef86a9689d11c4bbc2749268985098c8
Peter Shin <peshin@ebay.com>
2014-08-10 18:09:14 -0700

    Added keydown event handling.

--- js/text-search.js
+++ js/text-search.js
@@ -6,38 +6,36 @@
 		$displayEl = null,
 		$searchIndex = null,
 		$searchTotal =  null,
-		$selected = null,
 		$resultSet = null,
 		caseSensitive = true;
-
-	$(document).on('click','#text-search-extension li',selectCallback);
 
 	initDocEvents(document);
 
 	function selectCallback(){
 		var $this = $(this),
-			data = $this.data();
+			data = $this.data(),
+			$selected = getSelected();
 
-		clearSelect();
-		$selected = $this;
-		$selected.addClass('selected');
+		clearSelect($selected);
+		$this.addClass('selected');
 
 		if($resultSet.html() !== ''){
 			$searchIndex.text($this.index() + 1);
 			$searchTotal.text($this.siblings().length + 1);
 		}
 
-		data.textEl.html(data.textEl.html().replace(data.input,'<span class="ts-ce-hl">' + data.input + '</span>'))
+		data.textEl.html(data.textEl.html().replace(data.input,'<span class="ts-ce-hl">' + data.input + '</span>'));
 
 		$('html, body').animate({
-          scrollTop: data.textEl.offset().top - 100
+          scrollTop: data.offsetTop - 100
           },400);
 	}
 
-	function clearSelect(){
-		if($selected){
+	function clearSelect($el){
+		var $selected = $el || getSelected();
+		if($selected.length > 0){
 			var data = $selected.data();
-			data.textEl.html(data.textEl.html().replace('<span class="ts-ce-hl">' + data.input + '</span>',data.input))
+			data.textEl.html(data.textEl.html().replace('<span class="ts-ce-hl">' + data.input + '</span>',data.input));
 			$selected.removeClass('selected');
 		}
 
@@ -47,92 +45,145 @@
 	}
 
 	function isVisible(element) {
+	  var style;
 	  while (element) {
 	    style = window.getComputedStyle(element);
-	    if (style && (style.getPropertyValue('display') == 'none' ||
-	                  style.getPropertyValue('visibility') == 'hidden'))
-	      return false;
+	    if (style && (style.getPropertyValue('display') == 'none' || style.getPropertyValue('visibility') == 'hidden')){
+			return false;
+	    }
 	    element = element.parentNode;
 	  }
 	  return true;
 	}
 
+	function getSelected(){
+		return $('#text-search-extension li.selected');
+	}
+
+	//add settimeout
+
+	var keypressHandler = null,
+		keydownHandlerLi = null;
+
 	function initDocEvents(doc){
-		$(doc).on('keydown',function(evt){
-			if(evt.which === 191 && $(evt.target).is('body')){
+		$(doc).on('click','#text-search-extension li',selectCallback)
+		.on('keydown','#text-search-extension li', function(evt){
+
+			if(keydownHandlerLi){
+				clearTimeout(keydownHandlerLi);
+			}
+
+			var $this = $(this);
+
+			keydownHandlerLi = setTimeout(function(){
+				var $prev = null,
+					$next = null;
+
+				if(evt.which === 38){ //up
+					$prev = $this.prev();
+					if($prev.length > 0){
+						$prev.trigger('click').focus();
+					} else {
+						$('#text-search-extension input').focus();
+					}
+				} else if(evt.which === 40){ //down
+					$next = $this.next();
+					if($next.length > 0){
+						$next.trigger('click').focus();
+					}
+				}
+				return false;
+			},100);
+
+		}).on('keydown',function(evt){
+			console.log(evt);
+			if((evt.which === 191 || evt.which === 222 || (evt.which === 70 && evt.altKey === true)) && $(evt.target).is('body')){
 				toggleMenu();
 				return false;
 			}
 		});
 	}
 
-	//add settimeout
-
-	var keypressHandler = null;
-
 	function initSearchEvents(){
-		 $displayEl.on('keydown','input', function(evt){
-		 	evt.stopPropagation();
-
-		 	if(keypressHandler){
-		 		clearTimeout(keypressHandler);
-		 	}
-
-		 	var $this = $(this);
-
-		 	keypressHandler = setTimeout(function(){
-
-				var $ul = $displayEl.find('ul'),
-					li;
-
-				clearSelect();
-				$selected = null;
-				$ul.html('');
-
-			 	input = $this.val();
-
-			 	if(input.trim() === ''){
-			 		return;
-			 	}
-
-				var nodeIterator = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT,textMatch);
-				var results = [];
-
-				while ((textNode = nodeIterator.nextNode()) != null) {
-					results.push(textNode);
-				}
-
-				for(var i=0; i<results.length; i++){
-					var $parent = $(results[i]).parent();
-
-					$li = $('<li role="menuitem" tabindex="-1"></li>');
-					$li.text(results[i].data)
-						.html($li.html().replace(input,'<span class="ts-ce-hl">' + input + '</span>'))
-
-					if(!$selected){
-						$selected = $li;
-						$selected.addClass('selected');
-					}
-
-					$li.data({
-						textEl: $parent,
-						input: input
-					});
-
-					$ul.append($li);
-				}
-
-				if($selected){
-					$selected.trigger('click');
-				}
-
-		 	}, 50);
-
-
-		 }).on('click','i',toggleMenu);
+		 $displayEl.on('keydown','input', searchKeydownCb).on('click','i',toggleMenu);
 	}
 
- 	//update with regex
+	function searchKeydownCb(evt){
+		evt.stopPropagation();
+		//check for invalid key inputs and return
+
+		if(keypressHandler){
+			clearTimeout(keypressHandler);
+		}
+
+		var $this = $(this);
+
+		keypressHandler = setTimeout(function(){
+			var $prev = null,
+				$next = null,
+				$selected = getSelected();
+
+			if($selected.length > 0 && (evt.which === 38 || evt.which === 40)){
+				if(evt.which === 38){ //up
+					$prev = $selected.prev();
+					if($prev.length > 0){
+						$prev.trigger('click').focus();
+					}
+				} else if(evt.which === 40){ //down
+					$next = $selected.next();
+					if($next.length > 0){
+						$next.trigger('click').focus();
+					}
+				}
+				return false;
+			}
+
+			var $ul = $displayEl.find('ul'),
+				$li;
+
+			clearSelect();
+			$ul.html('');
+
+			input = $this.val();
+
+			if(input.trim() === ''){
+				return;
+			}
+
+			var nodeIterator = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT,textMatch),
+				results = [],
+				textNode = null;
+
+			while ((textNode = nodeIterator.nextNode()) != null) {
+				results.push(textNode);
+			}
+
+			for(var i=0; i<results.length; i++){
+				var $parent = $(results[i]).parent();
+
+				$li = $('<li role="menuitem" tabindex="0"></li>');
+				$li.text(results[i].data)
+					.html($li.html().replace(input,'<span class="ts-ce-hl">' + input + '</span>'));
+
+				$li.data({
+					textEl: $parent,
+					offsetTop: $parent.offset().top,
+					input: input
+				});
+
+				if(i === 0){
+					$li.addClass('selected');
+				}
+
+				$ul.append($li);
+			}
+
+			getSelected().trigger('click');
+
+		}, 150);
+	}
+
+	//update with regex
 	function textMatch(node){
 		var data = node ? node.data : '',
 			parent = node ? node.parentNode : null;
