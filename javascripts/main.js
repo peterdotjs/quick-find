69b2d1c056f725487f71a86cd61538d99ee5ba48
Peter Shin <peshin@ebay.com>
2015-03-21 20:45:32 -0700

    Adding in inline installs

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,33 @@
-console.log('This would be the main JS file.');
+(function($){
+
+  var isChrome = (window.chrome && window.chrome.webstore),
+      isOpera = (window.opr && window.opr.addons);
+
+  var chromeInstallBtn = $('#install-button-chrome'),
+      operaInstalBtn = $('#install-button-opera');
+
+  if(isChrome){
+    chromeInstallBtn
+      .attr({
+        role:'button',
+        href:'#'
+      });
+
+    chromeInstallBtn.on('click',function(evt){
+      evt.preventDefault();
+      chrome.webstore.install();
+    });
+
+  } else if(isOpera){
+    operaInstalBtn
+      .attr({
+        role:'button',
+        href:'#'
+      });
+
+    operaInstalBtn.on('click',function(evt){
+      evt.preventDefault();
+      opr.addons.installExtension('alfmlcakakiobbadicdfojldmmanlecg');
+    });
+  }
+})(window.jQuery);
